6b3c89dfb09c8e5700094c717071f5862dd29839
Ella <267187064+ella-acsc@users.noreply.github.com>
2026-07-29 00:22:50 +0000

    .

--- azul_plugin_python/decompiler/python_decompiler.py
+++ azul_plugin_python/decompiler/python_decompiler.py
@@ -15,7 +15,7 @@ import xdis.load
 
 
 class DecompileResults(TypedDict):
-    """Decompile results to keep pipeline happy"""
+    """Decompile results to keep pipeline happy."""
     error_type: str
     error_msg: str
     source: bytes
